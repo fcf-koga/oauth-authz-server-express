209d0c7a1b5ec634150612ec75fb39e4b8a6176c
fcf-koga <k-koga@future-creation-f.co.jp>
2024-07-28 16:27:52 +0900

    トークンエンドポイントの修正

--- routes/index.js
+++ routes/index.js
@@ -182,6 +182,10 @@ router.post("/approve", function (req, res, next) {
 
 // トークンエンドポイント
 router.post("/token", function (req, res, next) {
+  /*
+  クライアント認証情報取得
+  */
+
   // クライアント情報をAuthorizationヘッダーに登録してきた場合
   const auth = req.headers["authorization"];
   let clientId;
@@ -204,7 +208,10 @@ router.post("/token", function (req, res, next) {
     clientSecret = req.body.client_secret;
   }
 
-  // クライアント認証処理
+  /*
+  パラメータチェック
+  client_id[任意]
+  */
   const client = getClient(clientId);
 
   if (!client) {
@@ -218,83 +225,93 @@ router.post("/token", function (req, res, next) {
   }
 
   /*
-  必須パラメータチェック
-  */
+  パラメータチェック
+  grant_type[必須]
+   */
+  if (req.body.grant_type) {
+    // 認可コードグラントの場合
+    if (req.body.grant_type === "authorization_code") {
+      /*
+      パラメータチェック
+      code[必須]
+      */
+      const requests = req.session.requests[req.body.code];
+      delete req.session.requests[req.body.code];
+      // 前回のセッションで使用した認可コードと同じ場合
+      if (requests) {
+        // 前回のセッションで渡されたclient_idと同じかチェック
+        if (requests.client_id === clientId) {
+          // トークンの生成
+          const access_token = randomstring.generate();
+          const refresh_token = randomstring.generate();
 
-  // 認可コードグラントの場合
-  if (req.body.grant_type === "authorization_code") {
-    const requests = req.session.requests[req.body.code];
-    delete req.session.requests[req.body.code];
-    // 前回のセッションで使用した認可コードと同じ場合
-    if (requests) {
-      // 前回のセッションで渡されたclient_idと同じかチェック
-      if (requests.client_id === clientId) {
-        // トークンの生成
-        const access_token = randomstring.generate();
-        const refresh_token = randomstring.generate();
+          nosql.insert({
+            access_token: access_token,
+            client_id: clientId,
+            scope: requests.scope,
+          });
+          nosql.insert({
+            refresh_token: refresh_token,
+            client_id: clientId,
+            scope: requests.scope,
+          });
 
-        nosql.insert({
-          access_token: access_token,
-          client_id: clientId,
-          scope: requests.scope,
-        });
-        nosql.insert({
-          refresh_token: refresh_token,
-          client_id: clientId,
-          scope: requests.scope,
-        });
-
-        res.status(200).json({
-          access_token: access_token,
-          refresh_token: refresh_token,
-          token_type: "Bearer",
-          scope: requests.scope.join(" "),
-        });
-        return;
+          res.status(200).json({
+            access_token: access_token,
+            refresh_token: refresh_token,
+            token_type: "Bearer",
+            scope: requests.scope.join(" "),
+          });
+          return;
+        }
+        // 異なる場合
+        else {
+          // エラーを返す
+          res.status(400).json({ error: "invalid_grant" });
+          return;
+        }
       }
-      // 異なる場合
+      // 前回のセッションで使用した認可コードと異なる場合
       else {
         // エラーを返す
         res.status(400).json({ error: "invalid_grant" });
         return;
       }
     }
-    // 前回のセッションで使用した認可コードと異なる場合
-    else {
-      // エラーを返す
-      res.status(400).json({ error: "invalid_grant" });
-      return;
-    }
-  }
-  // リフレッシュトークンの場合
-  else if (res.body.grant_type === "refresh_token") {
-    nosql.one().make(function (builder) {
-      builder.where("refresh_token", req.body.refresh_token);
-      builder.callback(function (err, token) {
-        if (token) {
-          if (token.client_id != clientId) {
-            nosql.remove().make(function (builder) {
-              builder.where("refresh_token", req.body.refresh_token);
-            });
+    // リフレッシュトークンの場合
+    else if (req.body.grant_type === "refresh_token") {
+      nosql.one().make(function (builder) {
+        builder.where("refresh_token", req.body.refresh_token);
+        builder.callback(function (err, token) {
+          if (token) {
+            if (token.client_id != clientId) {
+              nosql.remove().make(function (builder) {
+                builder.where("refresh_token", req.body.refresh_token);
+              });
+              res.status(400).json({ error: "invalid_grant" });
+              return;
+            }
+
+            const access_token = randomstring.generate();
+            nosql.insert({ access_token: access_token, client_id: clientId });
+            const token_response = {
+              access_token: access_token,
+              token_type: "Bearer",
+              refresh_token: token.refresh_token,
+            };
+            res.status(200).json(token_response);
+            return;
+          } else {
             res.status(400).json({ error: "invalid_grant" });
             return;
           }
-
-          const access_token = randomstring.generate();
-          nosql.insert({ access_token: access_token, client_id: clientId });
-          const token_response = {
-            access_token: access_token,
-            token_type: "Bearer",
-            refresh_token: token.refresh_token,
-          };
-          res.status(200).json(token_response);
-          return;
-        } else {
-          res.status(400).json({ error: "invalid_grant" });
-          return;
-        }
+        });
       });
-    });
+    } else {
+      // エラーを返す
+      res.status(400).json({ error: "unsupported_grant_type" });
+      return;
+    }
   }
   // それ以外の付与法新規の場合
   else {
